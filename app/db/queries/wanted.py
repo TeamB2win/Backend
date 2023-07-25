7fbe8158adca3de58a02901bb869c1b49a652637
Magenta195 <novelistoblige@gmail.com>
2023-07-26 04:13:56 +0900

    Test : change join method

--- app/db/queries/wanted.py
+++ app/db/queries/wanted.py
@@ -5,17 +5,17 @@ from sqlalchemy.orm import joinedload
 from app.db.repositories.wanted import Wanted, WantedDataSource, WantedDetail
 
 async def get_full_wanted_data(db: AsyncSession) -> List[Wanted]:
-    query = select(Wanted, WantedDetail, WantedDataSource)
-    query = query.options(joinedload(WantedDetail, Wanted.id == WantedDetail.id))
-    query = query.options(joinedload(WantedDataSource, Wanted.id == WantedDataSource.id))
+    query = select(Wanted)
+    query = query.options(joinedload(Wanted.detail))
+    query = query.options(joinedload(Wanted.datasource))
 
     result = await db.execute(query)
     return result.scalars().all()
 
 async def get_wanted_data(db: AsyncSession, id : int) -> List[Wanted]:
-    query = select(Wanted, WantedDetail, WantedDataSource)
-    query = query.options(joinedload(WantedDetail, Wanted.id == WantedDetail.id))
-    query = query.options(joinedload(WantedDataSource, Wanted.id == WantedDataSource.id))
+    query = select(Wanted)
+    query = query.options(joinedload(Wanted.detail))
+    query = query.options(joinedload(Wanted.datasource))
     query = query.filter(Wanted.id == id)
     result = await db.execute(query)
     return [ result.scalars().first() ]
