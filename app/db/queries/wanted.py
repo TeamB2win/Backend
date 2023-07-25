ecb3d9b4aea688cf6221d606a65eb6cf33953bc9
Magenta195 <novelistoblige@gmail.com>
2023-07-26 04:00:16 +0900

    Feat : Change lazy join method

--- app/db/queries/wanted.py
+++ app/db/queries/wanted.py
@@ -1,20 +1,21 @@
 from typing import List
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
+from sqlalchemy.orm import joinedload
 from app.db.repositories.wanted import Wanted, WantedDataSource, WantedDetail
 
 async def get_full_wanted_data(db: AsyncSession) -> List[Wanted]:
     query = select(Wanted, WantedDetail, WantedDataSource)
-    query = query.join(WantedDetail, Wanted.id == WantedDetail.id)
-    query = query.join(WantedDataSource, Wanted.id == WantedDataSource.id)
+    query = query.options(joinedload(WantedDetail, Wanted.id == WantedDetail.id))
+    query = query.options(joinedload(WantedDataSource, Wanted.id == WantedDataSource.id))
 
     result = await db.execute(query)
     return result.scalars().all()
 
 async def get_wanted_data(db: AsyncSession, id : int) -> List[Wanted]:
     query = select(Wanted, WantedDetail, WantedDataSource)
-    query = query.join(WantedDetail, Wanted.id == WantedDetail.id)
-    query = query.join(WantedDataSource, Wanted.id == WantedDataSource.id)
+    query = query.options(joinedload(WantedDetail, Wanted.id == WantedDetail.id))
+    query = query.options(joinedload(WantedDataSource, Wanted.id == WantedDataSource.id))
     query = query.filter(Wanted.id == id)
     result = await db.execute(query)
     return [ result.scalars().first() ]
