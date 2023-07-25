e1bcf59940e7b4c65dbc4bf26ca0d5b2b1170769
Magenta195 <novelistoblige@gmail.com>
2023-07-26 04:45:15 +0900

    Debug : Do something

--- app/db/queries/wanted.py
+++ app/db/queries/wanted.py
@@ -1,21 +1,20 @@
 from typing import List
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-from sqlalchemy.orm import joinedload
 from app.db.repositories.wanted import Wanted, WantedDataSource, WantedDetail
 
 async def get_full_wanted_data(db: AsyncSession) -> List[Wanted]:
-    query = select(Wanted)
-    query = query.options(joinedload(Wanted.detail))
-    query = query.options(joinedload(Wanted.datasource))
+    query = select(Wanted, WantedDetail, WantedDataSource)
+    query = query.join(WantedDetail, Wanted.id == WantedDetail.id)
+    query = query.join(WantedDataSource, Wanted.id == WantedDataSource.id)
 
     result = await db.execute(query)
     return result.scalars().all()
 
 async def get_wanted_data(db: AsyncSession, id : int) -> List[Wanted]:
-    query = select(Wanted)
-    query = query.options(joinedload(Wanted.detail))
-    query = query.options(joinedload(Wanted.datasource))
+    query = select(Wanted, WantedDetail, WantedDataSource)
+    query = query.join(WantedDetail, Wanted.id == WantedDetail.id)
+    query = query.join(WantedDataSource, Wanted.id == WantedDataSource.id)
     query = query.filter(Wanted.id == id)
     result = await db.execute(query)
     return [ result.scalars().first() ]
--- app/models/schemas/wanted.py
+++ app/models/schemas/wanted.py
@@ -1,15 +1,15 @@
 from typing import Optional, List
 from app.models.schemas.base import BaseSchemaModel
-from app.models.domain.wanted import WantedData
+from app.models.domain.wanted import WantedFullData
 
 class WantedDataResponse(BaseSchemaModel) :
     data_hash : str
-    data : List[WantedData]
+    data : List[WantedFullData]
 
 class ListOfWantedDataResponse(BaseSchemaModel) :
     data_hash : str
-    data : List[WantedData]
+    data : List[WantedFullData]
 
 class OptionalListOfWantedDataResponse(BaseSchemaModel) :
     data_hash : str
-    data : Optional[List[WantedData]]
+    data : Optional[List[WantedFullData]]
