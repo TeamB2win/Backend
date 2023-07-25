b793017d0d1bb6bf84273e2173888c66493fb276
Magenta195 <novelistoblige@gmail.com>
2023-07-26 01:25:18 +0900

    Fix : await 적용

--- app/db/queries/wanted.py
+++ app/db/queries/wanted.py
@@ -3,15 +3,18 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from app.db.repositories.wanted import Wanted, WantedDataSource, WantedDetail
 
-def get_full_wanted_data(db: AsyncSession) -> List[Wanted]:
+async def get_full_wanted_data(db: AsyncSession) -> List[Wanted]:
     query = select(Wanted)
     query = query.join(WantedDetail, Wanted.id == WantedDetail.id)
     query = query.join(WantedDataSource, Wanted.id == WantedDataSource.id)
-    return db.execute(query).scalars().all()
 
-def get_wanted_data(db: AsyncSession, id : int) -> List[Wanted]:
+    result = await db.execute(query)
+    return result.scalars().all()
+
+async def get_wanted_data(db: AsyncSession, id : int) -> List[Wanted]:
     query = select(Wanted)
     query = query.join(WantedDetail, Wanted.id == WantedDetail.id)
     query = query.join(WantedDataSource, Wanted.id == WantedDataSource.id)
     query = query.filter(Wanted.id == id)
-    return [ db.execute(query).scalars().first() ]
+    result = await db.execute(query)
+    return [ result.scalars().first() ]
