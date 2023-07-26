8f57e3d2f08f3e90c282d5dcbb4ad704bddda1e4
Magenta195 <novelistoblige@gmail.com>
2023-07-26 10:56:52 +0900

    Fix : 쿼리 구조 수정

--- app/db/queries/wanted.py
+++ app/db/queries/wanted.py
@@ -6,16 +6,16 @@ from app.db.repositories.wanted import Wanted, WantedDataSource, WantedDetail
 
 async def get_full_wanted_data(db: AsyncSession) -> List[Wanted]:
     query = select(Wanted) \
-            .options(joinedload(Wanted.detail, innerjoin=True) \
-                     .joinedload(Wanted.datasource, innerjoin=True))
-
+            .options(joinedload(Wanted.detail, innerjoin=True)) \
+            .options(joinedload(Wanted.datasource, innerjoin=True))
+    
     result = await db.execute(query)
     return result.scalars().all()
 
 async def get_wanted_data(db: AsyncSession, id : int) -> List[Wanted]:
     query = select(Wanted) \
-            .options(joinedload(Wanted.detail, innerjoin=True) \
-                     .joinedload(Wanted.datasource, innerjoin=True)) \
+            .options(joinedload(Wanted.detail, innerjoin=True)) \
+            .options(joinedload(Wanted.datasource, innerjoin=True)) \
             .filter(Wanted.id == id)
     result = await db.execute(query)
     return result.scalars().all()
