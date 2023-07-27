5e1e26d14b7d5bec860f17c6a82fdd93315cd533
Magenta195 <novelistoblige@gmail.com>
2023-07-27 18:07:13 +0900

    !HOTFIX  : hash 함수 수정

--- app/secure/hash.py
+++ app/secure/hash.py
@@ -27,7 +27,8 @@ def to_str(
         data_dict = data.__dict__  # 객체의 멤버변수를 딕셔너리로 변환
         sorted_dict = OrderedDict(sorted(data_dict.items(), key=lambda x: x[0]))
         for key, value in sorted_dict.items():
-            sorted_dict[key] = to_str(value)
+            if isinstance(value, list) : 
+                sorted_dict[key] = to_str(value)
         data_str = str(sorted_dict).encode('utf-8')
     return data_str
 
