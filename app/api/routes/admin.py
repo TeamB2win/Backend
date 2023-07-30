02c3be47d96552e3726a8849b1ed026b10a7d798
moongni <mooooongni@gmail.com>
2023-07-30 21:36:01 +0900

    Feat: image upload admin api 분리기존 admin에서 image와 여러 컬럼을 동시에 받는 것에 문제 발생image만 따로 upload api 함수 분리데이터 필드를 추가후 DL_server로 요청보내는 부분 가작성 테스트 예정

--- app/api/routes/admin.py
+++ app/api/routes/admin.py
@@ -1,16 +1,17 @@
 import os
-import hashlib
 import io
+import datetime, secrets
 
-from PIL import Image 
+import asyncio
 import imageio
-from fastapi import APIRouter, Body, Depends, status
+from fastapi import APIRouter, Body, Depends, status, UploadFile
 from sqlalchemy.ext.asyncio import AsyncSession
+import httpx
 
 from app.db.events import get_db
 from app.db.queries.admin import create_wanted_data
 from app.db.repositories.wanted import Wanted, WantedDetail, WantedDataSource
-from app.models.schemas.admin import CreateWantedDataRequest, CreateWantedDataResponse
+from app.models.schemas.admin import *
 from app.secure.hash import generate_data_hash
 from app.api.errors.http_errors import HTTP_Exception
 from app.resources import strings
@@ -18,43 +19,50 @@ from app.resources import strings
 
 router = APIRouter(prefix = "/admin", tags = ["admin"])
 
-ImageReadException = HTTP_Exception(
+ImageWriteException = HTTP_Exception(
     status_code=status.HTTP_400_BAD_REQUEST,
     description=strings.DSCRIPTION_400_ERROR_FOR_DATA_FILE,
-    detail="Can't Read the image"
+    detail="Can't write the image"
 )
 ImageOpenException = HTTP_Exception(
     status_code=status.HTTP_400_BAD_REQUEST,
     description=strings.DSCRIPTION_400_ERROR_FOR_DATA_FILE,
     detail="Can't open image by using image framework(imageio)"
 )
+ImagePathException = HTTP_Exception(
+    status_code=status.HTTP_400_BAD_REQUEST,
+    description=strings.DSCRIPTION_400_ERROR_FOR_DATA_FILE,
+    detail="Can't find image path"
+)
 
-# 데이터 추가
+IMAGE_DIR = '/home/moongni/git/workspace/image'
+# Image upload
 @router.post(
-    path = "", 
+    path="/uploadimage",
     status_code=status.HTTP_201_CREATED,
-    response_model=CreateWantedDataResponse,
+    response_model=UploadImageResponse,
     responses={
-        **ImageReadException.responses
+        **ImageWriteException.responses,
+        **ImageOpenException.responses
     }
 )
-async def create_data(
-    request : CreateWantedDataRequest,
-    db_session : AsyncSession = Depends(get_db),
-) -> CreateWantedDataResponse:
+async def upload_image(
+    file: UploadFile
+) -> UploadImageResponse:
     # get image name
-    m = hashlib.sha256(f"source_{request.wanted_id}_{request.started_at}".encode('utf-8'))    
-    image_path = os.path.join("/workspace/data", m.hexdigest() + '.png')
-    # decode bytes to image
+    current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    image_path = os.path.join(IMAGE_DIR, ''.join([current_time, secrets.token_hex(16)]))
     try:
-        image = Image.open(request.image.file)
-        image.save(image_path)
+        image_type = file.content_type.split('/')[-1]
+        image_path += f".{image_type}"
+        with open(image_path, 'wb+') as file_object:
+            file_object.write(file.file.read())
     except:
         if os.path.exists(image_path):
             os.remove(image_path)
         print("Fail to Read the Image bytes")
-        ImageReadException.error_raise()
-    
+        ImageWriteException.error_raise()
+
     # check to open Image by paths
     try:
         imageio.imread(image_path) 
@@ -62,25 +70,76 @@ async def create_data(
         print("Fail to Open the Image using imageio")
         ImageOpenException.error_raise()
 
-    # Add to wanted Table in B2win DB  
-    wanted_data : Wanted = Wanted.create(request=request)
-    wanted_data : Wanted = await create_wanted_data(db=db_session, data_table=wanted_data)
+    return UploadImageResponse(
+        image_path=image_path,
+        status="OK"
+    )
 
-    # Add to wanted_detail Table in B2win DB  
-    wanted_detail_data : WantedDetail = WantedDetail.create(request=request, id=wanted_data.id)
-    wanted_detail_data : WantedDetail = await create_wanted_data(db=db_session, data_table=wanted_detail_data)
 
+# 데이터 추가
+@router.post(
+    path = "", 
+    status_code=status.HTTP_201_CREATED,
+    response_model=CreateWantedDataResponse,
+    responses={
+        **ImagePathException.responses
+    }
+)
+async def create_data(
+    request : CreateWantedDataRequest,
+    db_session : AsyncSession = Depends(get_db),
+) -> CreateWantedDataResponse:
+    # Validate image path
+    if not os.path.exists(request.image):
+        print(f"Invalid Image path {request.image}")
+        ImagePathException.error_raise()
+    
+    try:
+        # Add to wanted Table in B2win DB  
+        wanted_data : Wanted = Wanted.create(request=request)
+        wanted_data : Wanted = await create_wanted_data(db=db_session, data_table=wanted_data)
+
+        # Add to wanted_detail Table in B2win DB  
+        wanted_detail_data : WantedDetail = WantedDetail.create(request=request, id=wanted_data.id)
+        wanted_detail_data : WantedDetail = await create_wanted_data(db=db_session, data_table=wanted_detail_data)
+
+            
+        wanted_datasource_data : WantedDataSource = WantedDataSource.create(image_path=request.image, id=wanted_data.id)
+        wanted_datasource_data : WantedDataSource = await create_wanted_data(db=db_session, data_table=wanted_datasource_data)
+        print("insert data into table")
+    except:
+        # TODO: 테이블 내에 데이터가 생성되었다면 삭제 과정 필요
         
-    wanted_datasource_data : WantedDataSource = WantedDataSource.create(image_path=image_path, id=wanted_data.id)
-    wanted_datasource_data : WantedDataSource = await create_wanted_data(db=db_session, data_table=wanted_datasource_data)
-
-    data_hash : str = await generate_data_hash( db_session )
-
+        # Image 삭제
+        if os.path.exists(request.image):
+            os.remove(request.image)
+    
+    # DL server로 추론 요청
+    data = {
+        "id": wanted_data.id,
+        "wanted_type": wanted_data.wanted_type,
+        "image_path": wanted_datasource_data.image,
+        "prev_driving_path": wanted_datasource_data.driving_video,
+        "video_path": wanted_datasource_data.video
+    }
+    response = await request_dl_server(data)
+    print(response)
+                
+    data_hash: str = await generate_data_hash( db_session )
+    print("data_hash created")
+    
     return CreateWantedDataResponse(
         data_hash = data_hash,
         status = 'OK',
     )
 
+async def request_dl_server(data):
+    url = "http://63.35.31.27:8080/api/inference"
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, json=data)
+        print(response)
+
+    return response
 '''
 # 데이터 수정
 @router.patch("/app/{criminal_id}", status_code=200)
