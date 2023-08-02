20bf239a2b10640ebe13a070921013f4c7c1062c
Magenta195 <novelistoblige@gmail.com>
2023-08-02 22:45:17 +0900

    Feat : https run 기능 추가

--- app/run_https.sh
+++ app/run_https.sh
@@ -11,9 +11,14 @@ export FRONT_URL="http://63.35.31.27:3000"
 APP_MODULE="main:app"
 HOST="0.0.0.0"
 PORT="8000"
-
+KEY_PATH="./secure/key.pem"
+CERT_PATH="./secure/cert.pem"
 # Activate the virtual environment
 # source venv/bin/activate
 
+if [ ! -f "$KEY_PATH" ] || [ ! -f "$CERT_PATH" ]
+then
+    exec openssl req -x509 -newkey rsa:4096 -nodes -out "$CERT_PATH" -keyout "$KEY_PATH" -days 365
+fi
 # The program is run with the following command:
-exec uvicorn --reload --host "$HOST" --port "$PORT" "$APP_MODULE"
+exec uvicorn --reload  --ssl-keyfile="$KEY_PATH" --ssl-certfile="$CERT_PATH" --host "$HOST" --port "$PORT" "$APP_MODULE" 
