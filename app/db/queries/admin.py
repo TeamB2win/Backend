49f5cf4159e0e4dcf883051d0a9064dde7ca51d8
Magenta195 <novelistoblige@gmail.com>
2023-08-03 17:44:20 +0900

    Feat : Update API 추가

--- app/db/queries/admin.py
+++ app/db/queries/admin.py
@@ -1,9 +1,17 @@
+from typing import Optional, List, Any
+
 from sqlalchemy import select, update, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.repositories.base_class import Base
 
-from app.models.schemas.admin import UpdateWantedDataRequest, UpdateWantedVideoDataRequest
+from app.models.schemas.admin import UpdateWantedDataRequest, UpdateWantedMediaRequest
 from app.db.repositories.wanted import Wanted, WantedDataSource, WantedDetail
+
+table_dict = {
+    'wanted' : Wanted,
+    'wanted_datasource' : WantedDataSource,
+    'wanted_detail' : WantedDetail
+}
 
 # 범죄자 데이터 추가
 async def create_wanted_data(db : AsyncSession, data_table: Base) -> Base:
@@ -13,7 +21,7 @@ async def create_wanted_data(db : AsyncSession, data_table: Base) -> Base:
     # await db.commit()
     return data_table
 
-async def delete_wanted_data(db : AsyncSession, id: int) -> None:
+async def delete_wanted_data(db : AsyncSession, id: int) -> List[Any]:
     print("Query")
     wanted_datasource_query = delete(WantedDataSource).filter(WantedDataSource.id == id) \
         .returning(WantedDataSource.image, WantedDataSource.driving_video)
@@ -24,13 +32,25 @@ async def delete_wanted_data(db : AsyncSession, id: int) -> None:
     await db.execute(wanted_query)    
     return result.unique().scalars().all()
 
-# 범죄자 데이터 수정
-# async def update_wanted_data(db : AsyncSession, criminal_data: UpdateWantedVideoDataRequest) -> CreateWantedDataRequest:
-#     db.add(instance=criminal_data)
-#     db.commit()
-#     db.refresh(instance=criminal_data)
-#     return criminal_data
+async def update_wanted_data(db: AsyncSession, table_type: str, request : UpdateWantedDataRequest ) -> None :
+    table = table_dict[table_type]
+    query = update(table) \
+        .filter(table.id == request.id) \
+        .values({
+            **table.convert_dict(request)
+        })
+    await db.execute(query)
+    await db.flush()
+    return None
 
-# 범죄자 데이터 삭제
-# 
+async def update_wanted_datasource(db: AsyncSession, id : int, image_path : str ) -> List[Any] :
+    query = update(WantedDataSource) \
+        .filter(WantedDataSource.id == id) \
+        .values({
+            'image' : image_path,
+        }) \
+        .returning(WantedDataSource)
+    result = await db.execute(query)
+    await db.flush()
+    return result.first()[0]
 
